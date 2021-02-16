21216ad4587e82a572f1d0c57f7590c9441661b3
ericpinter <48108495+ericpinter@users.noreply.github.com>
2021-02-15 18:08:51 -0600

    Basic Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -10,6 +12,15 @@ public class GameManager : MonoBehaviour
     public GameObject dialogBox;
     public GameObject dialogText;
 
+    public GameObject menuText;
+    public GameObject startButton;
+    public GameObject backgroundImage;
+
+    public GameObject canvas;
+    public GameObject events;
+
+
+
     public Coroutine dialogcor;
 
     private void Awake()
@@ -18,6 +29,8 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(events);
         }
         else {
             Destroy(gameObject);
@@ -56,5 +69,54 @@ public class GameManager : MonoBehaviour
             dialogText.GetComponent<TextMeshProUGUI>().text += c;
             yield return new WaitForSeconds(0.1f);
         }
+    }
+    public void StartButton()
+    {
+        startButton.SetActive(false);
+        menuText.SetActive(false);
+        StartCoroutine(LoadYourAsyncScene("Snailtopia"));
+
+    }
+
+    public void GameOver()
+    {
+        startButton.SetActive(true);
+        StopAllCoroutines();
+        HideDialog();
+        StartCoroutine(ColorLerp(new Color(1, 1, 1, 1), 2));
+    }
+
+    IEnumerator ColorLerp(Color endValue, float duration)
+    {
+        float time = 0;
+        Image sprite = backgroundImage.GetComponent<Image>();
+        Color startValue = sprite.color;
+
+        while (time < duration)
+        {
+            sprite.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        sprite.color = endValue;
+    }
+
+    IEnumerator LoadYourAsyncScene(string scene)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+        print("trying to load "+scene);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(ColorLerp(new Color(1, 1, 1, 0), 2));
+
     }
 }
--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 * Snails : Slime snail by Tuomo Untinen https://opengameart.org/content/slime-snail 
 * Aztec Tileset : Created by Sevarihk https://sevarihk.itch.io/aztec-asset-pack
 * Mime : Created by Nelson Yiap https://opengameart.org/content/french-mime-with-beret-16x16-animated-spritesheet
+* Stamp : Made by us
